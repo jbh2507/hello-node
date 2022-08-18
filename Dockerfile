06fa175bf7d26ae007f2739cc8565a51b73c39d8
정보현 <bhjung@selab.co.kr>
2022-08-18 18:37:19 +0900

    [FEA] PM2로 실행하도록 변경
    PM2로 클러스터링하여 복수 인스턴스 올리도록 수정

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:16.17.0-alpine3.16
 WORKDIR /app
 COPY package*.json /app
 RUN npm install
+RUN npm install -g pm2@latest
 COPY . /app
-CMD [ "npm", "start" ]
+CMD [ "pm2-runtime", "start", "hello-node-pm2.config.js"]
 EXPOSE 80
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+console.log("node server up");
+
 let http = require('http');
+
+let port = process.env.PORT || '80'
+console.log("node forward port: " + port);
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'Application/json'});
     res.end('{status:OK}');
-}).listen(80);
+}).listen(port);
